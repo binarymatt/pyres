edd5b5d1d693b33d82d1fb91ec5830faeaacde12
Chris Song <chris__song@hotmail.com>
2009-12-10 13:38:11 +0800

    fix bugs

--- src/pyres/worker.py
+++ src/pyres/worker.py
@@ -35,12 +35,12 @@ class Worker(object):
     
     def _set_started(self, time):
         if time:
-            self.resq.redis.set("worker:%s:started" % self, time.strftime('%Y-%m-%d %H:%M:%S'))
+            self.resq.redis.set("resque:worker:%s:started" % self, time.strftime('%Y-%m-%d %H:%M:%S'))
         else:
-            self.resq.redis.delete("worker:%s:started" % self)
+            self.resq.redis.delete("resque:worker:%s:started" % self)
             
     def _get_started(self):
-        datestring = self.resq.redis.get("worker:%s:started" % self)
+        datestring = self.resq.redis.get("resque:worker:%s:started" % self)
         ds = None
         if datestring:
             ds = datetime.datetime.strptime(datestring, '%Y-%m-%d %H:%M:%S')
