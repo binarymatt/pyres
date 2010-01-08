e03d110df1159574710087a653acc0646ad66d56
Matt George <matt.george@myemma.com>
2010-01-08 09:57:04 -0600

    better setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
-from distutils.core import setup
-
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+version='0.4.1'
 setup(
     name='pyres',
-    version='0.4.1',
+    version=version,
     description='Python Resque clone',
     author='Matt George',
-    license='MIT',
     author_email='mgeorge@gmail.com',
+    maintainer='Matt George',
+    license='MIT',
     url='http://github.com/binarydud/pyres',
     packages=['pyres', 'resweb', 'pyres/failure'],
+    download_url='http://github.com/binarydud/pyres/tarball/v%s' % version,
     package_data={'resweb': ['templates/*.mustache','media/*']},
+    include_package_data=True,
     scripts=['scripts/pyres_worker', 'scripts/pyres_web'],
     zip_safe = True,
     install_requires=[
@@ -18,4 +24,11 @@ setup(
         'redis>=0.6.0',
         'pystache>=0.1.0'
     ],
+    classifiers = [
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python'],
 )
