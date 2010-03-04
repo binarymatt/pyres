04a0e87d7c513a6d91022bbd4fd305dc979f861d
Michael Russo <mjrusso@gmail.com>
2010-03-04 15:27:04 -0500

    correct type of keys now reported in Stats tab

--- resweb/views.py
+++ resweb/views.py
@@ -308,7 +308,7 @@ class Stat(ResWeb):
         return str(self.stat_id)
     
     def key_type(self):
-        return str(self.resq.redis.type(self.stat_id))
+        return str(self.resq.redis.type('resque:'+ str(self.stat_id)))
     
     def items(self):
         items = []
