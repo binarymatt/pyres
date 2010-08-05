1d454c1abd2a9ab3cdaf6d3536b9d77d0e5e09ec
Matt George <matt.george@myemma.com>
2010-08-05 10:46:50 -0500

    updating version number to 0.9

--- pyres/__init__.py
+++ pyres/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.8'
+__version__ = '0.9'
 
 from redis import Redis
 import pyres.json_parser as json
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
     
-version='0.8'
+version='0.9'
 setup(
     name='pyres',
     version=version,
