fea83d2ecde41c2bd4ccc2bcc1f1e89d8b3ea434
Craig Hawco <craig.hawco@gmail.com>
2010-11-11 14:23:47 -0500

    Ensure that failure backends are set correctly on job creation.

--- pyres/job.py
+++ pyres/job.py
@@ -1,36 +1,41 @@
 from datetime import datetime, timedelta
 from pyres import ResQ, str_to_class, safe_str_to_class
 from pyres import failure
+from pyres.failure.redis import RedisBackend
 
 class Job(object):
     """Every job on the ResQ is an instance of the *Job* class.
-    
+
     The ``__init__`` takes these keyword arguments:
-    
+
         ``queue`` -- A string defining the queue to which this Job will be added.
-    
+
         ``payload`` -- A dictionary which contains the string name of a class which extends this Job and
         a list of args which will be passed to that class.
-        
+
         ``resq`` -- An instance of the ResQ class.
-    
+
         ``worker`` -- The name of a specific worker if you'd like this Job to be done by that worker. Default is "None".
-    
+
     """
     def __init__(self, queue, payload, resq, worker=None):
         self._queue = queue
         self._payload = payload
         self.resq = resq
         self._worker = worker
-    
+
+        # Set the default back end, jobs can override when we import them
+        # inside perform().
+        failure.backend = RedisBackend
+
     def __str__(self):
         return "(Job{%s} | %s | %s)" % (
             self._queue, self._payload['class'], repr(self._payload['args']))
-    
+
     def perform(self):
         """This method converts payload into args and calls the ``perform`` method
         on the payload class.
-        
+
         """
         payload_class_str = self._payload["class"]
         payload_class = safe_str_to_class(payload_class_str)
@@ -41,11 +46,11 @@ class Job(object):
         except:
             if not self.retry(payload_class, args):
                 raise
-    
+
     def fail(self, exception):
         """This method provides a way to fail a job and will use whatever failure backend
         you've provided. The default is the ``RedisBackend``.
-        
+
         """
         fail = failure.create(exception, self._queue, self._payload, self._worker)
         fail.save(self.resq)
@@ -70,7 +75,7 @@ class Job(object):
     def reserve(cls, queue, res, worker=None):
         """Reserve a job on the queue. This marks this job so that other workers
         will not pick it up.
-        
+
         """
         payload = res.pop(queue)
         if payload:
