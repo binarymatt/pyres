278aba30b45d2b1e8231e13781c75256803dca48
Craig Hawco <craig.hawco@gmail.com>
2010-11-11 14:24:05 -0500

    Add some doc strings.

--- pyres/failure/multiple.py
+++ pyres/failure/multiple.py
@@ -2,6 +2,25 @@ from base import BaseBackend
 from redis import RedisBackend
 
 class MultipleBackend(BaseBackend):
+    """Extends ``BaseBackend`` to provide support for delegating calls to multiple
+    backends. Queries are delegated to the first backend in the list. Defaults to
+    only the RedisBackend.
+
+    To use:
+
+    from pyres import failure
+
+    from pyres.failure.base import BaseBackend
+    from pyres.failure.multiple import MultipleBackend
+    from pyres.failure.redis import RedisBackend
+
+    class CustomBackend(BaseBackend):
+        def save(self, resq):
+            print('Custom backend')
+
+            failure.backend = MultipleBackend
+            failure.backend.classes = [RedisBackend, CustomBackend]
+    """
     classes = []
 
     def __init__(self, *args):
