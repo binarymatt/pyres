22899c0583ff6b91b74b3486500d7fa285900a9f
Joe Shaw <joe@joeshaw.org>
2011-07-08 19:33:15 -0400

    add a -p flag for saving pidfiles in each of the scripts

--- pyres/scripts.py
+++ pyres/scripts.py
@@ -5,7 +5,7 @@ from optparse import OptionParser
 from itty import run_itty
 
 from pyres.horde import Khan
-from pyres import setup_logging
+from pyres import setup_logging, setup_pidfile
 from pyres.scheduler import Scheduler
 from resweb import server as resweb_server
 from pyres.worker import Worker
@@ -21,6 +21,7 @@ def pyres_manager():
     parser.add_option('-l', '--log-level', dest='log_level', default='info', help='log level.  Valid values are "debug", "info", "warning", "error", "critical", in decreasing order of verbosity. Defaults to "info" if parameter not specified.')
     parser.add_option("--pool", type="int", dest="pool_size", default=1, help="Number of minions to spawn under the manager.")
     parser.add_option('-f', dest='logfile', help='If present, a logfile will be used.')
+    parser.add_option('-p', dest='pidfile', help='If present, a pidfile will be used.')
     (options,args) = parser.parse_args()
 
     if len(args) != 1:
@@ -29,6 +30,8 @@ def pyres_manager():
 
     log_level = getattr(logging, options.log_level.upper(), 'INFO')
     #logging.basicConfig(level=log_level, format="%(asctime)s: %(levelname)s: %(message)s")
+
+    setup_pidfile(options.pidfile)
 
     interval = options.interval
     if interval is not None:
@@ -47,10 +50,12 @@ def pyres_scheduler():
     parser.add_option("--port",dest="port",type="int", default=6379)
     parser.add_option('-l', '--log-level', dest='log_level', default='info', help='log level.  Valid values are "debug", "info", "warning", "error", "critical", in decreasing order of verbosity. Defaults to "info" if parameter not specified.')
     parser.add_option('-f', dest='logfile', help='If present, a logfile will be used.')
+    parser.add_option('-p', dest='pidfile', help='If present, a pidfile will be used.')
     (options,args) = parser.parse_args()
     log_level = getattr(logging, options.log_level.upper(),'INFO')
     #logging.basicConfig(level=log_level, format="%(module)s: %(asctime)s: %(levelname)s: %(message)s")
     setup_logging(log_level=log_level, filename=options.logfile)
+    setup_pidfile(options.pidfile)
     server = '%s:%s' % (options.host, options.port)
     Scheduler.run(server)
 
@@ -90,6 +95,7 @@ def pyres_worker():
     parser.add_option("-i", '--interval', dest='interval', default=None, help='the default time interval to sleep between runs')
     parser.add_option('-l', '--log-level', dest='log_level', default='info', help='log level.  Valid values are "debug", "info", "warning", "error", "critical", in decreasing order of verbosity. Defaults to "info" if parameter not specified.')
     parser.add_option('-f', dest='logfile', help='If present, a logfile will be used.')
+    parser.add_option('-p', dest='pidfile', help='If present, a pidfile will be used.')
     (options,args) = parser.parse_args()
 
     if len(args) != 1:
@@ -98,6 +104,8 @@ def pyres_worker():
 
     log_level = getattr(logging, options.log_level.upper(), 'INFO')
     setup_logging(log_level=log_level, filename=options.logfile)
+    setup_pidfile(options.pidfile)
+
     interval = options.interval
     if interval is not None:
         interval = int(interval)
